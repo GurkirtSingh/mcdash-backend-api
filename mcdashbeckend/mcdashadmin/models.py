d5c82f776aa0c59eb11d36f6b8c4c73f322e2212
Gurkirt Singh <Gsingh2904@conestogac.on.ca>
2021-02-03 22:10:45 -0800

    created models
    Added models Store, Staff, Shift_Detail, Employee, Shift and Previouse_Shift_Detail

--- mcdashbeckend/mcdashadmin/models.py
+++ mcdashbeckend/mcdashadmin/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.deletion import CASCADE
+from django.db.models.expressions import Case
+from django.db.models.fields import DateTimeField
 
-# Create your models here.
+# STORE - Model
+class Store(models.Model):
+    store_number = models.CharField(max_length=100) # store id or uniqly identified number
+    name = models.CharField(max_length=100)
+
+# Staff - Model
+class Staff(models.Model):
+    store = models.ForeignKey(Store, on_delete=CASCADE)
+    position = models.CharField(max_length=100)
+
+# SHIFT_DETAIL - Model
+class Shift_Detail(models.Model):
+    start_at = models.DateTimeField()
+    end_at = models.DateTimeField()
+    station = models.CharField(max_length=30)
+    store = models.ForeignKey(Store, on_delete=models.CASCADE)
+
+# Employee - Model
+class Employee(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    store = models.ForeignKey(Store, on_delete=models.CASCADE)
+    staff = models.ForeignKey(Staff, on_delete=CASCADE)
+
+# Shift - model
+class Shift(models.Model):
+    employee = models.ForeignKey(Employee, on_delete=CASCADE)
+    shift_detail = models.ForeignKey(Shift_Detail, on_delete=CASCADE)
+
+class Previous_Shift_Detail(models.Model):
+    shift = models.ForeignKey(Shift, on_delete=CASCADE)
+    shift_detail = models.ForeignKey(Shift_Detail, on_delete=CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
