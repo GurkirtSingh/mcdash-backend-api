fc82b46f4ae7aae3d5827cb5af5266aa44ec8f8d
Gurkirt Singh <Gsingh2904@conestogac.on.ca>
2021-02-05 22:40:07 -0800

    slash('/') should be after the name

--- mcdashbeckend/mcdashadmin/urls.py
+++ mcdashbeckend/mcdashadmin/urls.py
@@ -3,6 +3,6 @@ from .views import admin_and_store_register, admin_login, admin_home
 
 urlpatterns = [
     path('', admin_and_store_register, name='admin_and_store_register'),
-    path('/login', admin_login, name='admin_login'),
-    path('/home', admin_home, name='admin_home'),
+    path('login/', admin_login, name='admin_login'),
+    path('home/', admin_home, name='admin_home'),
 ]
