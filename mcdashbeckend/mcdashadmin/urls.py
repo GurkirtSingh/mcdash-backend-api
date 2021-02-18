f12638c480a51d1d56ad95e88e7076e3411c3ed5
Gurkirt Singh <Gsingh2904@conestogac.on.ca>
2021-02-17 20:41:48 -0800

    added routes to login and home view

--- mcdashbeckend/mcdashadmin/urls.py
+++ mcdashbeckend/mcdashadmin/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import admin_and_store_register, admin_login, admin_home
+from .views import *
 
 urlpatterns = [
     path('', admin_and_store_register, name='admin_and_store_register'),
-    path('login/', admin_login, name='admin_login'),
-    path('home/', admin_home, name='admin_home'),
+    path('login', admin_login, name='admin_login'),
+    path('home', admin_home, name='admin_home'),
+    path('logout', admin_logout, name='admin_logout'),
+    path('change-shift', assign_new_shift_view, name='assign_new_shift'),
 ]
